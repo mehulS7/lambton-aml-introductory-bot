b7731aa1d2a18d2e88f5238d36923cae9ed3cdd5
Mehul Sachdeva <Mehul.Sachdeva7@gmail.com>
2021-08-18 01:57:13 -0400

    removed tensorflow

--- nlp.py
+++ nlp.py
@@ -5,7 +5,7 @@ import numpy as np
 import warnings
 import nltk
 from nltk.stem import WordNetLemmatizer
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 
 warnings.filterwarnings("ignore")
 nltk.download('punkt')
